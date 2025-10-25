9cee81f5de93fc8d56b9473eed0c25bf5a33a129
jpolan15 <jonathanpolanc4@gmail.com>
2025-10-25 18:58:31 -0400

    Add root URL redirect and sample polls data

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
+
+def redirect_to_polls(request):
+    return redirect('polls:index')
 
 urlpatterns = [
+    path("", redirect_to_polls, name="home"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
